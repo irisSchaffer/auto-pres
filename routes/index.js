2ffeae3cd57e7b699c03ac594bd167314da4d23c
Iris Schaffer <irisSchaffer@gmx.net>
2015-12-01 17:37:18 +0100

    start server

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.render('index', { images: [
+  res.render('presentation', { images: [
     "http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg",
     "http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
     "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg",
