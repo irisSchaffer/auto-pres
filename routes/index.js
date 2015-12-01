73b8682bf537ddcc46ecc1980cb6ef7b99c81a95
Benji1 <Benji1@users.noreply.github.com>
2015-12-01 18:37:39 +0100

    server checks tf for input and prints keywords back

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/', function(req, res, next) {
-  res.render('index', { images: [
-    "http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg",
-    "http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
-    "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg",
-  ] });
-});
-
-router.post('/slides', function(req, res, next) {
-	console.log(req.body);
-});
-
-module.exports = router;
-
 var AYLIENTextAPI = require("aylien_textapi");
 var textapi = new AYLIENTextAPI({
   application_id: "80889f4e",
   application_key: "48d80b752d6cd44405ade5da12080d06"
 });
 
-var text = "John is a very good football player!";
-
-textapi.sentiment({"text": text}, function(error, response) {
-  if (error === null) {
-    console.log(response);
-  }
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-textapi.language({"text": text}, function(error, response) {
-  if (error === null) {
-    console.log(response);
-  }
+router.post('/', function(req, res, next) {
+	var text = req.body['textinput'];
+	console.log(text);
+	
+	textapi.entities(text, function(error, response) {
+		var keys = "";
+		if (error === null) {
+			Object.keys(response.entities).forEach(function(e) {
+				keys += e + ": " + response.entities[e].join(",");
+				//console.log(e + ": " + response.entities[e].join(","));
+			});
+		}
+		console.log(keys);
+		res.render('index', { title: "Express", keywords: keys });
+	});
 });
+
+module.exports = router;
+
+
+
