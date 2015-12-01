87e00c9eca1ddd151a1ee2338c6c702992e84860
Iris Schaffer <irisSchaffer@gmx.net>
2015-12-01 18:14:46 +0100

    Added notes in

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,18 @@ router.get('/test', function(req, res, next) {
   {
       layout: '',
       images: [
-        "http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg",
-        "http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
-        "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg",
+          {
+              img_url: "http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg",
+              text: "Test Text 1"
+          },
+          {
+              img_url: "http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg",
+              text: "Test Text 2"
+          },
+          {
+              img_url: "http://blog.jimdo.com/wp-content/uploads/2014/01/tree-247122.jpg",
+              text: "Test Text 3"
+          }
       ]
   });
 });
