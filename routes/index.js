9ae3bc62ff9e3c87bf9f89b4fee288545aa84eab
Iris Schaffer <irisSchaffer@gmx.net>
2015-12-10 13:43:37 +0100

    front end improvements, added in bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+var AYLIENTextAPI = require("aylien_textapi");
+var textapi = new AYLIENTextAPI({
+  application_id: "80889f4e",
+  application_key: "48d80b752d6cd44405ade5da12080d06"
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'auto-presi' });
 });
 
 router.post('/', function(req, res, next) {
-    res.render('index', { title: 'auto-presi' });
+	var text = req.body['textinput'];
+	console.log(text);
+
+	textapi.entities(text, function(error, response) {
+		var keys = "";
+		if (error === null) {
+			Object.keys(response.entities).forEach(function(e) {
+				keys += e + ": " + response.entities[e].join(",");
+				//console.log(e + ": " + response.entities[e].join(","));
+			});
+		}
+		console.log(keys);
+        res.render('index', { title: 'auto-presi', keywords: keys });
+	});
 });
 
 router.get('/test', function(req, res, next) {
@@ -31,28 +50,4 @@ router.get('/test', function(req, res, next) {
   });
 });
 
-router.post('/slides', function(req, res, next) {
-	console.log(req.body);
-});
-
 module.exports = router;
-
-var AYLIENTextAPI = require("aylien_textapi");
-var textapi = new AYLIENTextAPI({
-  application_id: "80889f4e",
-  application_key: "48d80b752d6cd44405ade5da12080d06"
-});
-
-var text = "John is a very good football player!";
-
-textapi.sentiment({"text": text}, function(error, response) {
-  if (error === null) {
-    console.log(response);
-  }
-});
-
-textapi.language({"text": text}, function(error, response) {
-  if (error === null) {
-    console.log(response);
-  }
-});
